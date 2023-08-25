86cd1b2db8c762e9d05aae9915d505bdf1a0b782
Farhan <farhanahad3@gmail.com>
2023-08-25 11:37:05 +0600

    Error handling

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,66 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
 /* eslint-disable no-console */
-import cors from 'cors'
-import express, { Application, Request, Response } from 'express'
-const app: Application = express()
+import cors from 'cors';
+import express, { Application } from 'express';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
+import { UserRoutes } from './app/modules/users/user.route';
+//import globalErrorHandler from './app/middlewares/globalErrorHandler'
 
-app.use(cors())
+//import ApiError from './errors/ApiError'
+
+const app: Application = express();
+app.use(cors());
 
 //parser
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//APIError
+// app.get('/', (req: Request, res: Response) => {
+//   throw new ApiError(400, 'Custom error')
+// })
+
+// app.get('/', (_req: Request, _res: Response, next: NextFunction) => {
+//   next('Custom error')
+// })
 
 // console.log(app.get('env'))
 // console.log(process.env)
 //Testing
-app.get('/', (req: Request, res: Response) => {
-  res.send('Working Successfully')
-})
+// app.get('/', async (req: Request, res: Response) => {
+//   await usersService.createUser({
+//     id: '222',
+//     password: '12343',
+//     role: 'Student',
+//   })
+//   res.send('Working Successfully')
+// })
+//Application Routes
 
-export default app
+app.use('/api/v1/users/', UserRoutes);
+
+// app.get('/', async (req: Request, res: Response) => {
+//   res.send('Working Successfully')
+// })
+
+// //custom error
+// app.get('/', (_req: Request, res: Response) => {
+//   throw new ApiError(400, 'Custom error')
+// })
+
+//UnHandled Rejection
+// app.get('/', async (req: Request, res: Response) => {
+//   Promise.reject(new Error('Unhandled rejection'))
+// })
+
+//uncaught exception
+// app.get('/', async (req: Request, res: Response) => {
+//   console.log('x')
+// })
+
+//global error handler
+
+app.use(globalErrorHandler);
+export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,46 @@
-import mongoose from 'mongoose'
-import app from './app'
-import config from './config/index'
-import { errorLogger, logger } from './shared/logger'
+import { Server } from 'http';
+import mongoose from 'mongoose';
+import app from './app';
+import config from './config/index';
+import { errorLogger, logger } from './shared/logger';
+
+process.on('uncaughtException', error => {
+  errorLogger.error(error);
+  process.exit(1);
+});
+
+let server: Server;
 
 async function boostrap() {
   try {
-    await mongoose.connect(config.database_url as string)
-    logger.info(`🛢   Database is connected successfully`)
+    await mongoose.connect(config.database_url as string);
+    logger.info(`🛢   Database is connected successfully`);
 
-    app.listen(config.port, () => {
-      logger.info(`Application  listening on port ${config.port}`)
-    })
+    server = app.listen(config.port, () => {
+      logger.info(`Application  listening on port ${config.port}`);
+    });
   } catch (err) {
-    errorLogger.error('Failed to connect database', err)
+    errorLogger.error('Failed to connect database', err);
   }
+
+  //handle UnHandled Rejection
+  process.on('unhandledRejection', error => {
+    if (server) {
+      server.close(() => {
+        errorLogger.error(error);
+        process.exit(1);
+      });
+    } else {
+      process.exit(1);
+    }
+  });
 }
 
-boostrap()
+boostrap();
+
+process.on('SIGTERM', () => {
+  logger.info('SIGTERM is received');
+  if (server) {
+    server.close();
+  }
+});
